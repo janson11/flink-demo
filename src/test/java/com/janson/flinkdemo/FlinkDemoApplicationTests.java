6698b007058a9b51d97585df32c06cb004be18a6
shanjian <shanjian@codemao.com>
2022-02-07 15:25:30 +0800

    init

---
+++ src/test/java/com/janson/flinkdemo/FlinkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janson.flinkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
